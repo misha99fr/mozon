0b957100d2a2b24b6a5319adfec084f12f50e4af
igorkll <69525332+igorkll@users.noreply.github.com>
2022-07-03 15:24:56 +0300

    Update package.lua

--- system/core/lib/package.lua
+++ system/core/lib/package.lua
@@ -8,18 +8,16 @@ local package = {}
 package.libsPaths = {"/system/core/lib"}
 package.createEnv = createEnv
 
-function package.findLib(name)
-    
-end
-
 function _G.require(name)
     return package.loaded[name]
 end
 
-package.loaded = {
-    package = package,
-    paths = raw_dofile("/system/core/lib/paths.lua", nil, createEnv()),
-    filesystem = raw_dofile("/system/core/lib/filesystem.lua", nil, createEnv()),
-}
+function package.findLib(name)
+    
+end
+
+package.loaded = {package = package}
+package.loaded.paths = raw_dofile("/system/core/lib/paths.lua", nil, createEnv())
+package.loaded.filesystem = raw_dofile("/system/core/lib/filesystem.lua", nil, createEnv())
 
 return package
