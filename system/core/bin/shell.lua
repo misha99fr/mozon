eec697a282f98c98637f77a682d7997fb56d2b55
igorkll <69525332+igorkll@users.noreply.github.com>
2022-07-03 20:59:01 +0300

    Update shell.lua

--- system/core/bin/shell.lua
+++ system/core/bin/shell.lua
@@ -1,6 +1,8 @@
+local component = require("component")
+local calls = require("calls")
+
 local windows = {}
 do
-    local calls = require("calls")
     local graphicInit = calls.load("graphicInit")
 
     for address in component.list("screen") do
@@ -14,10 +16,19 @@ do
     end
 end
 
+local readers = {}
 for i, window in ipairs(windows) do
-    window:clear(math.random(0, 0xFFFFFF))
-    for i = 1, 3 do
-        window:write(tostring(i) .. "\n", math.random(0, 0xFFFFFF), math.random(0, 0xFFFFFF))
-        os.sleep(1)
+    window:clear(0x0000FF)
+    for i = 1, 2 do
+        window:write(tostring(i) .. "\n", 0xFFFF00, 0x00AAFF)
+    end
+    local cx, cy = window:getCursor()
+    table.insert(readers, window:read(cx, cy, window.sizeX, 0xFF00FF, 0x00FF00))
+end
+
+while true do
+    local eventData = {computer.pullSignal()}
+    for i, v in ipairs(readers) do
+        
     end
 end
