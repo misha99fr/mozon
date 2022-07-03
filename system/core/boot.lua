1948f0a2bfa9a00e16bf24c6f2202f86e52e64b8
igorkll <69525332+igorkll@users.noreply.github.com>
2022-07-03 17:33:36 +0300

    update

--- system/core/boot.lua
+++ system/core/boot.lua
@@ -40,16 +40,19 @@ end
 
 ----------------------------------
 
-local windows = {}
-for address in component.list("screen") do
-    local term = require("term")
-    local gpu = term.findGpu(address)
-    if gpu then
-        gpu.setBackground(0)
-        local rx, ry = gpu.getResolution()
-        gpu.fill(1, 1, rx, ry, " ")
-        table.insert(windows, term.classWindow:new(address, 1, 1, 25, 5))
-        table.insert(windows, term.classWindow:new(address, 1, 7, 25, 5))
+do
+    local calls = require("calls")
+    local graphicInit = calls.load("graphicInit")
+
+    local windows = {}
+    for address in component.list("screen") do
+        local term = require("term")
+        local gpu = term.findGpu(address)
+        if gpu then
+            graphicInit(gpu)
+            table.insert(windows, term.classWindow:new(address, 1, 1, 25, 5))
+            table.insert(windows, term.classWindow:new(address, 1, 7, 25, 5))
+        end
     end
 end
 
@@ -57,6 +60,6 @@ for i, window in ipairs(windows) do
     window:clear(math.random(0, 0xFFFFFF))
     for i = 1, 3 do
         window:write(tostring(i) .. "\n", math.random(0, 0xFFFFFF), math.random(0, 0xFFFFFF))
+        os.sleep(1)
     end
-    os.sleep(1)
 end
