3faa15dfd04c2b36d52267210ea38ced374dbfc3
igorkll <69525332+igorkll@users.noreply.github.com>
2022-07-03 21:51:30 +0300

    Update event.lua

--- system/core/lib/event.lua
+++ system/core/lib/event.lua
@@ -1,10 +1,16 @@
 local computer = require("computer")
-
+local fs = require("filesystem")
 
 ------------------------------------
 
 local event = {}
 event.listens = {}
+
+function event.tmpLog(data)
+    local file = assert(fs.open("/tmp/tmplog.log", "ab"))
+    file.write(data .. "\n")
+    file.close()
+end
 
 function event.pull()
     
@@ -32,11 +38,42 @@ function computer.pullSignal(time)
 
     local inTime = computer.uptime()
     while true do
-        local time = time - (computer.uptime() - inTime)
-        if time <= 0 then return end
+        local ltime = time - (computer.uptime() - inTime)
+        if ltime <= 0 then return end
+        local realtime = ltime
+
+        --поиск времени до первого таймера, что обязательно на него успеть
+        for k, v in pairs(event.listens) do --нет ipairs неподайдет
+            if v.type == "t" then
+                local timerTime = v.time - (computer.uptime() - v.lastTime)
+                if timerTime < realtime then
+                    realtime = timerTime
+                end
+            end
+        end
+
+        local eventData = {computer_pullSignal(realtime)} --обязательно повисеть в pullSignal
 
         for k, v in pairs(event.listens) do --нет ipairs неподайдет
-            if v.
+            if v.type == "t" then
+                local uptime = computer.uptime() 
+                if uptime - v.lastTime >= v.time then
+                    v.lastTime = uptime --ДО выполнения функции ресатаем таймер, чтобы тайминги не поплывали при долгих функциях
+                    local ok, err = pcall(v.func)
+                    if ok then
+                        if err == false then --таймер хочем отключиться
+                            event.listens[k] = nil
+                        end
+                    else
+                        event.tmpLog((err or "unknown error") .. "\n")
+                    end
+                end
+            end
+        end
+
+        if #eventData > 0 then
+
+            return table.unpack(eventData)
         end
     end
 end
