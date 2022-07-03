f3e8cd4d3ec55010e823f5a40c19b44f2b7a2e65
igorkll <69525332+igorkll@users.noreply.github.com>
2022-07-03 17:46:23 +0300

    Create split.lua

---
+++ system/core/calls/split.lua
@@ -0,0 +1,18 @@
+local function split(str, sep)
+    local parts, count, i = {}, 1, 1
+    while 1 do
+        if i > #str then break end
+        local char = str:sub(i, #sep + (i - 1))
+        if not parts[count] then parts[count] = "" end
+        if char == sep then
+            count = count + 1
+            i = i + #sep
+        else
+            parts[count] = parts[count] .. str:sub(i, i)
+            i = i + 1
+        end
+    end
+    if str:sub(#str - (#sep - 1), #str) == sep then table.insert(parts, "") end
+    return parts
+end
+return split(...)
